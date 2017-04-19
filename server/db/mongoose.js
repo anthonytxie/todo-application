9460b1650bfaaeb48a962d2af30ff2bc780a64e9
anthonytxie <anthony.xie@vidyard.com>
2017-04-18 23:26:50 -0400

    add mongodb-uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise; //telling mongoose to use native Promises
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
 
